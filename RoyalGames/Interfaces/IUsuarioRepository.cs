65d563ff9454a86bb83211fcd54292d16fbc8c8e
Raphael-Aljona <raphael.aljona@gmail.com>
2026-02-27 10:54:30 -0300

    Interface e Repository do Usuario feitas

---
+++ RoyalGames/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using RoyalGames.Domains;
+
+namespace RoyalGames.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        List<Usuario> GetUsuarios();
+        Usuario? GetUsuarioById(int id);
+        void Adicionar(Usuario usuario);
+        void Remover(int id);
+        void Atualizar (Usuario usuario);   
+    }
+}
--- RoyalGames/obj/Debug/net8.0/RoyalGames.AssemblyInfo.cs
+++ RoyalGames/obj/Debug/net8.0/RoyalGames.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoyalGames")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dee7109308692abee1ffd20ee2a069b73798ec9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dda1aad47ef7c5c510b5497e3302699d1f13873")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoyalGames")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoyalGames")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
