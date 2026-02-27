dee7109308692abee1ffd20ee2a069b73798ec9e
CATARINA KONDOLIZA GARDENIA FAGUNDES DE MARTIN <42332992820@info.local>
2026-02-27 09:50:29 -0300

    :tada: Criando pastas

---
+++ RoyalGames/Contexts/RoyalGamesContext.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using RoyalGames.Domains;
+
+namespace RoyalGames.Contexts;
+
+public partial class RoyalGamesContext : DbContext
+{
+    public RoyalGamesContext()
+    {
+    }
+
+    public RoyalGamesContext(DbContextOptions<RoyalGamesContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categoria> Categoria { get; set; }
+
+    public virtual DbSet<ClassificacaoIndicativa> ClassificacaoIndicativa { get; set; }
+
+    public virtual DbSet<Jogo> Jogo { get; set; }
+
+    public virtual DbSet<Log_AlteracaoJogo> Log_AlteracaoJogo { get; set; }
+
+    public virtual DbSet<Plataforma> Plataforma { get; set; }
+
+    public virtual DbSet<Usuario> Usuario { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=RoyalGames;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(entity =>
+        {
+            entity.HasKey(e => e.CategoriaID).HasName("PK__Categori__F353C1C5B2498A62");
+
+            entity.Property(e => e.Nome)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<ClassificacaoIndicativa>(entity =>
+        {
+            entity.HasKey(e => e.ClassificacaoIndicativaID).HasName("PK__Classifi__892DEC6F732F39E8");
+
+            entity.Property(e => e.Classificacao)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Jogo>(entity =>
+        {
+            entity.HasKey(e => e.JogoID).HasName("PK__Jogo__591968554655C1C8");
+
+            entity.ToTable(tb =>
+                {
+                    tb.HasTrigger("trg_AlteracaoJogo");
+                    tb.HasTrigger("trg_ExclusaoJogo");
+                });
+
+            entity.HasIndex(e => e.Nome, "UQ__Jogo__7D8FE3B20E43EAED").IsUnique();
+
+            entity.Property(e => e.Nome)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Preco).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.StatusJogo).HasDefaultValue(true);
+
+            entity.HasOne(d => d.ClassificacaoIndicativa).WithMany(p => p.Jogo)
+                .HasForeignKey(d => d.ClassificacaoIndicativaID)
+                .HasConstraintName("FK__Jogo__Classifica__534D60F1");
+
+            entity.HasOne(d => d.Usuario).WithMany(p => p.Jogo)
+                .HasForeignKey(d => d.UsuarioID)
+                .HasConstraintName("FK__Jogo__UsuarioID__52593CB8");
+
+            entity.HasMany(d => d.Categoria).WithMany(p => p.Jogo)
+                .UsingEntity<Dictionary<string, object>>(
+                    "JogoCategoria",
+                    r => r.HasOne<Categoria>().WithMany()
+                        .HasForeignKey("CategoriaID")
+                        .HasConstraintName("FK_JogoCategoria_Categoria"),
+                    l => l.HasOne<Jogo>().WithMany()
+                        .HasForeignKey("JogoID")
+                        .HasConstraintName("FK_JogoCategoria_Produto"),
+                    j =>
+                    {
+                        j.HasKey("JogoID", "CategoriaID");
+                    });
+
+            entity.HasMany(d => d.Plataforma).WithMany(p => p.Jogo)
+                .UsingEntity<Dictionary<string, object>>(
+                    "JogoPlataforma",
+                    r => r.HasOne<Plataforma>().WithMany()
+                        .HasForeignKey("PlataformaID")
+                        .HasConstraintName("FK_JogoPlataforma_Plataforma"),
+                    l => l.HasOne<Jogo>().WithMany()
+                        .HasForeignKey("JogoID")
+                        .HasConstraintName("FK_JogoPlataforma_Jogo"),
+                    j =>
+                    {
+                        j.HasKey("JogoID", "PlataformaID");
+                    });
+        });
+
+        modelBuilder.Entity<Log_AlteracaoJogo>(entity =>
+        {
+            entity.HasKey(e => e.Log_AlteracaoJogoID).HasName("PK__Log_Alte__BB9D2C4FD48ACC3B");
+
+            entity.Property(e => e.DataAlteracao).HasPrecision(0);
+            entity.Property(e => e.NomeAnterior)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.PrecoAnterior).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.Jogo).WithMany(p => p.Log_AlteracaoJogo)
+                .HasForeignKey(d => d.JogoID)
+                .HasConstraintName("FK__Log_Alter__JogoI__619B8048");
+        });
+
+        modelBuilder.Entity<Plataforma>(entity =>
+        {
+            entity.HasKey(e => e.PlataformaID).HasName("PK__Platafor__B835678DC312776D");
+
+            entity.Property(e => e.Nome)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.UsuarioID).HasName("PK__Usuario__2B3DE7988669E85B");
+
+            entity.ToTable(tb => tb.HasTrigger("trg_ExclusaoUsuario"));
+
+            entity.Property(e => e.AdminUsuario).HasDefaultValue(false);
+            entity.Property(e => e.Email)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.Nome)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+            entity.Property(e => e.StatusUsuario).HasDefaultValue(true);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
